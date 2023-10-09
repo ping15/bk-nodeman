d76c01658748de15076950643690c1688f3eb955
wyyalt <wyyalt@live.com>
2023-10-09 10:13:13 +0800

    feature:  提供 Agent 包管理后台接口 (closed #1683)

--- apps/core/tag/targets/agent.py
+++ apps/core/tag/targets/agent.py
@@ -11,8 +11,11 @@ specific language governing permissions and limitations under the License.
 
 
 import logging
+import typing
 
 from apps.core.tag.models import Tag
+from apps.node_man.constants import BUILT_IN_TAG_NAMES
+from apps.node_man.models import GsePackageDesc
 
 from .. import constants
 from . import base
@@ -26,12 +29,41 @@ class AgentTargetHelper(base.BaseTargetHelper):
     TARGET_TYPE = constants.TargetType.AGENT.value
 
     def _publish_tag_version(self):
-        Tag.objects.update_or_create(
-            defaults={"target_version": self.target_version},
-            name=self.tag_name,
-            target_id=self.target_id,
-            target_type=self.TARGET_TYPE,
-        )
+        if self.tag_name in BUILT_IN_TAG_NAMES:
+            Tag.objects.update_or_create(
+                defaults={"target_version": self.target_version},
+                name=self.tag_name,
+                target_id=self.target_id,
+                target_type=self.TARGET_TYPE,
+            )
+            return
+
+        try:
+            tag = Tag.objects.filter(
+                name=self.tag_name,
+                target_id=self.target_id,
+                target_type=self.TARGET_TYPE,
+            ).first()
+
+            if not tag.target_version:
+                tag.target_version = self.target_version
+                tag.save()
+            else:
+                Tag.objects.update_or_create(
+                    name=self.tag_name,
+                    target_id=self.target_id,
+                    target_type=self.TARGET_TYPE,
+                    target_version=self.target_version,
+                    description=tag.description,
+                )
+
+        except Tag.DoesNotExist:
+            pass
 
     def _delete_tag_version(self):
-        return super()._delete_tag_version()
+        pass
+
+    @classmethod
+    def get_agent_name_target_id_map(cls) -> typing.Dict[str, int]:
+        package_descs = GsePackageDesc.objects.values("project", "id")
+        return {package_desc["project"]: package_desc["id"] for package_desc in package_descs}
